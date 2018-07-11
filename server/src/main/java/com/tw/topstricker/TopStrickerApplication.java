435ed75ca3193e2c5fe0190203f1f50d9d4daa6f
Zhao Liu <zhliu@thoughtworks.com>
2018-07-11 16:51:08 +0800

    [Server] Init project.

---
+++ server/src/main/java/com/tw/topstricker/TopStrickerApplication.java
@@ -0,0 +1,12 @@
+package com.tw.topstricker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopStrickerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TopStrickerApplication.class, args);
+    }
+}
