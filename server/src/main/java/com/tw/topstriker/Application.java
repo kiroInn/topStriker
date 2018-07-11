10f9dd1265516f4fc288edd25725d9955499ff22
Zhao Liu <zhliu@thoughtworks.com>
2018-07-11 22:38:17 +0800

    [Server] Fix the project name.

--- server/src/main/java/com/tw/topstriker/Application.java
+++ server/src/main/java/com/tw/topstriker/Application.java
@@ -1,12 +1,12 @@
-package com.tw.topstricker;
+package com.tw.topstriker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TopStrickerApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(TopStrickerApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
--- server/src/test/java/com/tw/topstriker/ApplicationTests.java
+++ server/src/test/java/com/tw/topstriker/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.tw.topstricker;
+package com.tw.topstriker;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,10 +7,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TopStrickerApplicationTests {
+public class ApplicationTests {
 
     @Test
     public void contextLoads() {
+
     }
 
 }
